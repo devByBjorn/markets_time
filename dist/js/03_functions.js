9b4aaae5203f322b91f09d22ffc3ae82ff619699
devByBjorn <devbybjorn@gmail.com>
2020-01-14 16:47:00 +0100

    seconds hide and show option

--- dist/js/03_functions.js
+++ dist/js/03_functions.js
@@ -1,24 +1,41 @@
+// Ticking seconds and 'hide seconds' as textContent when opening page
 (function tickingSeconds() {
-  let seconds = moment().format("ss")
+  let seconds = moment().format('ss')
 
-  document.querySelector("#seconds").textContent = seconds
+  document.querySelector('#seconds').textContent = seconds
   setTimeout(tickingSeconds, 1000)
 }())
 
 
+// Function for hide, show and change textContent
+const addhideShow = (element, hideOrShow) => {
+  document.querySelector(element).classList.add(hideOrShow)
+}
 
-// style backgroundColor
-// const setBackgroundColor = (id, color) => {
-//   document.querySelector(`#${id}`).style.backgroundColor = color
-// }
+const removeAddSHow = (element, hideOrShow) => {
+  document.querySelector(element).classList.remove(hideOrShow)
+}
 
-// //style color
-// const setColor = (id, color) => {
-//   document.querySelector(`#${id}`).style.color = color
-// }
+const changeContent = (element, content) => {
+  document.querySelector(element).textContent = content
+}
 
-// //textContent
-// const setTextContent = (id, content) => {
-//   document.querySelector(`#${id}`).textContent = content
-// }
 
+// Hide or show seconds when checking checkbox
+document.querySelector('#control-seconds').addEventListener('change', (e) => {
+  if (e.target.checked) {
+    addhideShow('#seconds', 'hide')
+    removeAddSHow('#seconds', 'show')
+    changeContent('.hide-show', 'Show')
+  } else if (!e.target.checked) {
+    addhideShow('#seconds', 'show')
+    removeAddSHow('#seconds', 'hide')
+    changeContent('.hide-show', 'Hide')
+  }
+})
+
+changeContent('.hide-show', 'Hide')
+
+
+// Come up with solution making sure checkbox always is 
+//unchecked and with content 'Hide' on window load
